41e3ab89f3f9081be60ab53f272b63d7d362696e
Rampagy <alexjsimon32@gmail.com>
2023-12-02 00:37:32 -0600

    Updating language comparison bat file

--- compare_languages_windows.bat
+++ compare_languages_windows.bat
@@ -1,6 +1,4 @@
 @echo off
 
 .\C++\build\djikstras_search_Windows.exe
-:: java -jar .\Java\astar_search.jar
-:: .\C#\builds\astar_search.exe
-:: python .\Python\main.py
+.\Rust\target\release\rust_dijkstras.exe
